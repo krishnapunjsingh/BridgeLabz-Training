fdda346f793c3ecb32d6fff0441ab3b3092940c1
Krishna punj singh <krishnapunjsingh@gmail.com>
2025-12-17 15:44:48 +0530

    [Krishna Punj Singh].add problems

---
+++ core-java-practice/gcr-codebase/programming-elements/AreaOfCircle.java
@@ -0,0 +1,7 @@
+Class Main {
+    public static void main(String[] args) {
+        double radius = 7.5;
+        double area = Math.PI * radius * radius;
+        System.out.println("The area of the circle with radius " + radius + " is: " + area);
+    }
+}

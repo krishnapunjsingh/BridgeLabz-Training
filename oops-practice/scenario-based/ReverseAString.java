25926bd6696d856a9ee606ce65ee60ac86f4445e
Krishna punj singh <krishnapunjsingh@gmail.com>
2026-01-02 15:34:28 +0530

    [Krishna Punj Singh] Add . Added scenario based questions

---
+++ oops-practice/scenario-based/ReverseAString.java
@@ -0,0 +1,8 @@
+class ReverseAString {
+    public static void main(String[] args) {
+        String input = "Java";
+        for(int i=input.length()-1;i>=0;i--){
+            System.out.print(input.charAt(i));
+        }
+    }
+}   

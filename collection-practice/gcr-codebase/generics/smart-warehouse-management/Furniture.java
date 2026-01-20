67457c543f575414bf5217709c100428ab6331f4
Krishna punj singh <krishnapunjsingh@gmail.com>
2026-01-20 16:28:40 +0530

    [krishna punj singh] Add . Added generics questions

---
+++ collection-practice/gcr-codebase/generics/smart-warehouse-management/Furniture.java
@@ -0,0 +1,11 @@
+public class Furniture extends WarehouseItem {
+
+    public Furniture(String name, double price) {
+        super(name, price);
+    }
+
+    @Override
+    public String getCategory() {
+        return "Furniture";
+    }
+}
---
+++ collection-practice/gcr-codebase/generics/smart-warehouse-management/Groceries.java
@@ -0,0 +1,11 @@
+public class Groceries extends WarehouseItem {
+
+    public Groceries(String name, double price) {
+        super(name, price);
+    }
+
+    @Override
+    public String getCategory() {
+        return "Groceries";
+    }
+}

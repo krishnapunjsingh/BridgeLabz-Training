a285d1f18904424b47a12f432b6e3d976c4a25d1
Krishna punj singh <krishnapunjsingh@gmail.com>
2026-02-03 21:59:34 +0530

    [krishna punj singh] Add . Added scenario based

---
+++ collection-practice/scenario-based/stream-buzz/CreatorStats.java
@@ -0,0 +1,17 @@
+public class CreatorStats {
+	String creatorName;
+    double[] weeklyLikes = new double[4];
+
+    public CreatorStats(String creatorName, double[] weeklyLikes) {
+        this.creatorName = creatorName;
+        this.weeklyLikes = weeklyLikes;
+    }
+
+    double calculateAverageLikes() {
+        double sum = 0;
+        for (double like : weeklyLikes) {
+            sum += like;
+        }
+        return sum / 4;
+    }
+}
